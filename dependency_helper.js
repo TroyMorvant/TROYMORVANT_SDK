b70c748a3543487b77479d20b330255fda795d70
Troy Morvant <troy.morvant@gmail.com>
2022-04-28 02:53:17 -0500

    feat: add dependency charts

--- dependency_helper.js
+++ dependency_helper.js
@@ -8,17 +8,14 @@ madge('src/index.ts').then((res) => {
   res.image('dependency_graph.svg').then((writtenImagePath) => {
     console.log('Image written to ' + writtenImagePath);
   });
-  res.image('libs/dependency_graph.svg').then((writtenImagePath) => {
-    console.log('Image written to ' + writtenImagePath);
-  });
 });
 
 // Sub Modules
-readdirSync('libs', { withFileTypes: true })
+readdirSync('src', { withFileTypes: true })
   .filter((dir) => dir.isDirectory())
   .map((dir) => {
-    madge(`libs/${dir.name}/index.ts`)
-      .then((res) => res.image(`libs/${dir.name}/dependency_graph.svg`))
+    madge(`src/${dir.name}/index.ts`)
+      .then((res) => res.image(`src/${dir.name}/dependency_graph.svg`))
       .then((writtenImagePath) => {
         console.log('Image written to ' + writtenImagePath);
       });
